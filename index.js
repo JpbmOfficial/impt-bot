72d7c419c8c8011484ae338c1314488df3a12bcc
JpbmOfficial <31043326+JpbmOfficial@users.noreply.github.com>
2018-04-03 19:19:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,14 +10,6 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-  let prefix = botconfig.prefix;
-    if(!message.content.startsWith(`${prefix}`)) return;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
-
 });
 
 client.login(process.env.BOT_TOKEN);
